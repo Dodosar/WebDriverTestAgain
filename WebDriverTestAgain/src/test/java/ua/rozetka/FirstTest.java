57409cd29873468279405fb6f8b62f2fac5a3227
Dodosar <depechemode1989@mail.ru>
2017-11-21 16:40:34 +0200

    Added click to Test =2

--- WebDriverTestAgain/src/test/java/ua/rozetka/FirstTest.java
+++ WebDriverTestAgain/src/test/java/ua/rozetka/FirstTest.java
@@ -3,12 +3,15 @@ package ua.rozetka;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+
+import com.gargoylesoftware.htmlunit.javascript.host.dom.Document;
 
 public class FirstTest {
 	
@@ -25,6 +28,7 @@ public class FirstTest {
   @Test(priority = 1)
   public void GoToLink(){
 	  System.out.println("Will go to link Black Friday");
+	  driver.findElement(By.xpath("//*[@id='3361']//a")).click();
   }
   
   @BeforeTest
