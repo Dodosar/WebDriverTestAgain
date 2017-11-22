c616c1b1c13d27e68d44b62fced7e69b1abc96a7
Dodosar <depechemode1989@mail.ru>
2017-11-22 13:05:27 +0200

    Format File

--- WebDriverTestAgain/src/test/java/ua/rozetka/FirstTest.java
+++ WebDriverTestAgain/src/test/java/ua/rozetka/FirstTest.java
@@ -1,6 +1,5 @@
 package ua.rozetka;
 
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -16,72 +15,72 @@ import org.testng.annotations.AfterTest;
 import com.gargoylesoftware.htmlunit.javascript.host.dom.Document;
 
 public class FirstTest {
-	
-  private WebDriver driver;
-  
-  @Test(priority = 0)
-  public void f() {
-	  	driver.get("https://rozetka.com.ua/");  
-	  	driver.manage().window().maximize();
+
+	private WebDriver driver;
+
+	@Test(priority = 0)
+	public void f() {
+		driver.get("https://rozetka.com.ua/");
+		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println(title);
-		try{
-		Assert.assertTrue(title.equals("Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие"));
-		System.out.println("Test Pass");
+		try {
+			Assert.assertTrue(title
+					.equals("Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие"));
+			System.out.println("Test Pass");
+		} catch (Exception e) {
+			System.out.println("Test Faild");
 		}
-		  catch(Exception e){
-			  System.out.println("Test Faild");
-		  }
-  }
-  
-  @Test(priority = 1)
-  public void GoToLink(){
-	  driver.findElement(By.xpath("//a[@data-title='Смартфоны, ТВ и электроника']"));
-	  driver.findElement(By.xpath("//a[@data-title='Смартфоны, ТВ и электроника']")).click();
-	 /* String title1 = driver.getTitle();
-	  System.out.println(title1);
-	  try{
-	  Assert.assertTrue(title1.equals("Телефоны, ТВ и электроника - Rozetka.ua | Купить Телефоны, ТВ и электроника в Киеве: цена, отзывы, продажа"));
-	  System.out.println("Test Pass");
-	  }
-	  catch(Exception e){
-		  System.out.println("Test Faild");
-	  }*/
-  }
-  
-  /*@Test(priority = 2)
-  public void GoToSmarphones(){
-	  driver.findElement(By.xpath("//p[contains(concat(' ', @class,' '), ' pab-h3 ')]/a[@href='https://rozetka.com.ua/telefony/c4627900/']")).click();
-	  String title = driver.getTitle();
-	  System.out.println(title);
-  }*/
-  
-  @BeforeTest
-  @Parameters("browser")
-  public void setUp(String browser) throws Exception {
-	  		if(browser.equalsIgnoreCase("chrome")){
-	  		System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
-	 		driver = new ChromeDriver();	
-	  		}
-	  		else if(browser.equalsIgnoreCase("firefox")){
-	  			System.setProperty("webdriver.gecko.driver", ".\\geckodriver.exe");
-	  			driver = new FirefoxDriver();
-	  		}
-	  		else{
-	  			throw new Exception("Browser is Not Correct");
-	  		}
-	 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-	 		/*
-			 * 	System.out.println("Welcome to Maven World");
-	    		System.setProperty("webdriver.gecko.driver", ".\\geckodriver.exe");
-	    		driver = new FirefoxDriver();       
-	    		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-			 */
-  }
+	}
 
-  @AfterTest
-  public void Close() {
-	 // driver.quit();
-  }
+	@Test(priority = 1)
+	public void GoToLink() {
+		driver.findElement(By
+				.xpath("//a[@data-title='Смартфоны, ТВ и электроника']"));
+		driver.findElement(
+				By.xpath("//a[@data-title='Смартфоны, ТВ и электроника']"))
+				.click();
+		/*
+		 * String title1 = driver.getTitle(); System.out.println(title1); try{
+		 * Assert.assertTrue(title1.equals(
+		 * "Телефоны, ТВ и электроника - Rozetka.ua | Купить Телефоны, ТВ и электроника в Киеве: цена, отзывы, продажа"
+		 * )); System.out.println("Test Pass"); } catch(Exception e){
+		 * System.out.println("Test Faild"); }
+		 */
+	}
+
+	/*
+	 * @Test(priority = 2) public void GoToSmarphones(){
+	 * driver.findElement(By.xpath(
+	 * "//p[contains(concat(' ', @class,' '), ' pab-h3 ')]/a[@href='https://rozetka.com.ua/telefony/c4627900/']"
+	 * )).click(); String title = driver.getTitle(); System.out.println(title);
+	 * }
+	 */
+
+	@BeforeTest
+	@Parameters("browser")
+	public void setUp(String browser) throws Exception {
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", ".\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			throw new Exception("Browser is Not Correct");
+		}
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		/*
+		 * System.out.println("Welcome to Maven World");
+		 * System.setProperty("webdriver.gecko.driver", ".\\geckodriver.exe");
+		 * driver = new FirefoxDriver();
+		 * driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		 */
+	}
+
+	@AfterTest
+	public void Close() {
+		// driver.quit();
+	}
 
 }
